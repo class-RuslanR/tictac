af57e8d2eacf7efdcf6050a03cfea4e765b68a29
Ruslan Rogozhkin <biz.ruslan@gmail.com>
2020-07-16 11:17:31 +0300

    first commit

---
+++ build/CMakeFiles/TicTac.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "F:/C++/IT_Academy/Homeworks/HW6(TicTac)/TicTacToe_code/board.cpp" "F:/C++/IT_Academy/Homeworks/HW6(TicTac)/TicTacToe_code/build/CMakeFiles/TicTac.dir/board.cpp.obj"
+  "F:/C++/IT_Academy/Homeworks/HW6(TicTac)/TicTacToe_code/game.cpp" "F:/C++/IT_Academy/Homeworks/HW6(TicTac)/TicTacToe_code/build/CMakeFiles/TicTac.dir/game.cpp.obj"
+  "F:/C++/IT_Academy/Homeworks/HW6(TicTac)/TicTacToe_code/main.cpp" "F:/C++/IT_Academy/Homeworks/HW6(TicTac)/TicTacToe_code/build/CMakeFiles/TicTac.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/TicTac.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TicTac.dir/board.cpp.obj"
+  "CMakeFiles/TicTac.dir/game.cpp.obj"
+  "CMakeFiles/TicTac.dir/main.cpp.obj"
+  "TicTac.exe"
+  "TicTac.exe.manifest"
+  "TicTac.pdb"
+  "libTicTac.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TicTac.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
